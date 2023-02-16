26795c7ba14b747d47336333ce0e604a0d3ca793
DrCopyPaste <gg.link@web.de>
2023-02-16 22:06:44 +0100

    add flyout menu and about page displaying version

---
+++ src/RoutinR.MAUI/AboutPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace RoutinR.MAUI
+{
+    public partial class AboutPage : ContentPage
+    {
+        int count = 0;
+
+        public AboutPage()
+        {
+            InitializeComponent();
+            VersionLabel.Text = $"Version {ThisAssembly.AssemblyFileVersion} / {ThisAssembly.AssemblyInformationalVersion}";
+        }
+    }
+}
