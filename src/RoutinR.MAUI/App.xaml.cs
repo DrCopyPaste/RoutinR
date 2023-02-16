d963dba229306a911da650952770b8f4f3f825b1
DrCopyPaste <gg.link@web.de>
2023-02-16 20:43:44 +0100

    add nerdbank gitversioning, add default maui template for ui

---
+++ src/RoutinR.MAUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace RoutinR.MAUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ src/RoutinR.MAUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace RoutinR.MAUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
