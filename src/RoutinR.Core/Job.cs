213e415fc90fdd05df5852f7db64f7ab5ae2bf54
DrCopyPaste <gg.link@web.de>
2023-03-29 22:10:36 +0200

    Equals/GetHashCode for Job

--- src/RoutinR.Core/Job.cs
+++ src/RoutinR.Core/Job.cs
@@ -1,4 +1,5 @@
 ﻿using RoutinR.Constants;
+using System.Reflection.PortableExecutable;
 
 namespace RoutinR.Core
 {
@@ -6,6 +7,32 @@ namespace RoutinR.Core
     {
         private readonly string name = JobNames.Idle;
         public string Name => name;
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is not Job that) return false;
+            if (this == null ||  that == null) return this == that;
+
+            return this.Name == that.Name;
+        }
+
+        /// <summary>
+        /// Get HashCode of this instance
+        /// 
+        /// e.g. see https://stackoverflow.com/a/263416
+        /// </summary>
+        public override int GetHashCode()
+        {
+            unchecked // Overflow is fine, just wrap
+            {
+                // pick 2 prime numbers, the number to multiply by should be large
+                int hash = 17;
+
+                hash = hash * 486187739 + Name.GetHashCode();
+
+                return hash;
+            }
+        }
 
         private Job() { }
 
